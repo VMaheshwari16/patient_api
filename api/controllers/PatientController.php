2ee6565c4b94cf6dd86ecb36fbcc5c6e66635624
VMaheshwari16 <131899919+VMaheshwari16@users.noreply.github.com>
2026-02-05 18:06:57 +0530

    Update PatientController.php

--- api/controllers/PatientController.php
+++ api/controllers/PatientController.php
@@ -19,30 +19,65 @@ class PatientController {
                 $result = $id
                     ? $this->patient->getById($id)
                     : $this->patient->getAll();
-
-                Response::json(true, "Patients fetched", $result);
+                if (!$result) {
+                    http_response_code(404);
+                    Response::json(false, "Patient not found");
+                }
+                else{
+                    Response::json(true, "Patients fetched", $result);
+                }
                 break;
 
             case 'POST':
-                if (empty($data['name']) || empty($data['age'])) {
+                if (empty($data['name']) || empty($data['age']) || empty($data['gender']) || empty($data['phone']))
+                {
                     http_response_code(400);
                     Response::json(false, "Invalid input data");
                 }
                 else{
-                $this->patient->create($data);
-                http_response_code(201);
-                Response::json(true, "Patient created",$data);
+                    $this->patient->create($data);
+                    http_response_code(201);
+                    Response::json(true, "Patient created",$data);
                 }
                 break;
 
             case 'PUT':
+                if (!$id) {
+                    http_response_code(400);
+                    Response::json(false, "Patient ID required");
+                }
+
+                if (empty($data['name']) || empty($data['age']) || empty($data['gender']) || empty($data['phone']))
+                {
+                    http_response_code(400);
+                    Response::json(false, "All fields are required");
+                }
                 $this->patient->update($id, $data);
-                Response::json(true, "Patient updated");
+                Response::json(true, "Patient updated",$data);
                 break;
 
             case 'DELETE':
+                if (!$id) {
+                    http_response_code(400);
+                    Response::json(false, "Patient ID required");
+                }
                 $this->patient->delete($id);
                 Response::json(true, "Patient deleted");
+                break;
+            
+            case 'PATCH':
+                if (!$id) {
+                    http_response_code(400);
+                    Response::json(false, "Patient ID required");
+                }
+
+                if (empty($data)) {
+                    http_response_code(400);
+                    Response::json(false, "No fields provided to update");
+                }
+
+                $this->patient->patch($id, $data);
+                Response::json(true, "Patient updated partially");
                 break;
 
             default:

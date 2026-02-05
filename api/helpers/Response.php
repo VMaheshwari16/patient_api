aa523b3850117db00ea8d8d06f40329e03c12b5f
VMaheshwari16 <131899919+VMaheshwari16@users.noreply.github.com>
2026-02-05 15:56:30 +0530

    Create Response.php

---
+++ api/helpers/Response.php
@@ -0,0 +1,14 @@
+<?php
+
+class Response
+{
+    public static function json($status, $message, $data = [])
+    {
+        echo json_encode([
+            "status"  => $status,
+            "message" => $message,
+            "data"    => $data
+        ]);
+        exit;
+    }
+}

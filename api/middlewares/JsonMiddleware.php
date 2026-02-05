e694a09decf29b66b5ee80c57cc69c0f747a445b
VMaheshwari16 <131899919+VMaheshwari16@users.noreply.github.com>
2026-02-05 18:07:37 +0530

    Update JsonMiddleware.php

--- api/middlewares/JsonMiddleware.php
+++ api/middlewares/JsonMiddleware.php
@@ -6,7 +6,7 @@ class JsonMiddleware
     {
         header("Content-Type: application/json");
         header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+        header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS");
         header("Access-Control-Allow-Headers: Content-Type");
 
         if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
